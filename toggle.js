2efba25081184ec1d061a7dd7fde121767fca581
Allen Nguyen <anhn321@gmail.com>
2022-05-25 15:39:06 -0700

    Toggle players stats when button pressed.

---
+++ toggle.js
@@ -0,0 +1,9 @@
+function select(player) {
+    // get the selected player's details and button, as well as the old details and button
+    let stats = document.getElementById(player + "-stats");
+    let button = document.getElementById(player + "-button");
+
+    // toggle the player's details visibility and selection status
+    stats.classList.toggle("invisible");
+    button.classList.toggle("selected");
+}
