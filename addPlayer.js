16dff5a75a9c826ae98b2acea92384ad90c06fef
Allen Nguyen <anhn321@gmail.com>
2022-05-26 14:02:06 -0700

    Removed debugging print statements.

--- addPlayer.js
+++ addPlayer.js
@@ -1,11 +1,10 @@
 //addPlayer('lebron', 'Lebron James', 'images\\Lebron James.jpg', 27.1, 4, 5, 18, 'Most playoff points: 7631');
 addPlayer('kareem', 'Kareem Abdul-Jabbar', 'images\\Kareem Abdul-Jabbar.jpg', 24.6, 6, 2, 15, 'Most MVPs: 6');
 
-
 // Adds a player to index.html from the given player information
 function addPlayer(playerFirstName, playerFullName, imgPath, ppg, nbaChamp, scoringChamp, allNBA, funFact) {
     let playersList = document.getElementById("all-players");
-    console.log(playersList);
+    
     let newPlayer = createDOM(
         `<div class="goat ` + playerFirstName + `"> 
             <button id="` + playerFirstName + `-button" class=` + playerFirstName + ` onclick="select('` + playerFirstName + `');"> 
@@ -23,12 +22,12 @@ function addPlayer(playerFirstName, playerFullName, imgPath, ppg, nbaChamp, scor
             </div>
         </div>`
     );
-    console.log(newPlayer);
+
     playersList.appendChild(newPlayer);
 }
 
 // Creates a DOM element from the given HTML string
-function createDOM (htmlString){
+function createDOM (htmlString) {
     let template = document.createElement('template');
     template.innerHTML = htmlString.trim();
     return template.content.firstChild;
